4705ca3c305d40d705c9068ad151a307a8171fc0
SongKyuSeob <sks5075@naver.com>
2025-10-21 22:29:42 +0900

    init: 프로젝트 init

---
+++ VoiceRecordApp/VoiceRecordApp/VoiceRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceRecordApp.swift
+//  VoiceRecordApp
+//
+//  Created by 송규섭 on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct VoiceRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
